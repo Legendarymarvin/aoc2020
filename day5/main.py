3ed8c42d534839ec9903cf477ab04235a58965c2
Default Profile <mail@metz.eu.com>
2020-12-05 08:25:52 +0100

    Actual Day 5 solution (first commit was old version, had only part1)

--- day5/main.py
+++ day5/main.py
@@ -1,4 +1,4 @@
-def binary_search_id(partition):
+def convert_to_binary_and_int(partition):
     partition = partition.replace('F', '0')
     partition = partition.replace('L', '0')
     partition = partition.replace('B', '1')
@@ -9,23 +9,39 @@ def binary_search_id(partition):
 def calculate_id(boardingpass):
     row_part = boardingpass[:7]
     column_part = boardingpass[-4:]
-    row = binary_search_id(row_part)
-    column = binary_search_id(column_part)
+    row = convert_to_binary_and_int(row_part)
+    column = convert_to_binary_and_int(column_part)
     return row * 8 + column
+
+
+def create_seat_ids(boarding_passes):
+    seat_ids = []
+    for boarding_pass in boarding_passes:
+        seat_id = calculate_id(boarding_pass)
+        seat_ids.append(seat_id)
+
+    seat_ids.sort()
+    return seat_ids
+
+
+def get_your_id(seat_ids_sorted):
+    id = 0
+    for seat_id, idx in enumerate(seat_ids_sorted):
+        if seat_ids_sorted[idx] + 1 < seat_ids_sorted[idx + 1]:
+            id = seat_ids_sorted[idx] + 1
+            break
+    return id
 
 
 def day_5():
     with open('input', 'r') as f:
-        boardingpasses = f.readlines()
+        boarding_passes = f.readlines()
 
-    max_id = 0
+    seat_ids_sorted = create_seat_ids(boarding_passes)
+    print("Part 1: " + str(seat_ids_sorted[-1]))
 
-    for boardingpass in boardingpasses:
-        seat_id = calculate_id(boardingpass)
-        if seat_id > max_id:
-            max_id = seat_id
-
-    print(max_id)
+    your_id = get_your_id(seat_ids_sorted)
+    print("Part 2: " + str(your_id))
 
 
 if __name__ == '__main__':
